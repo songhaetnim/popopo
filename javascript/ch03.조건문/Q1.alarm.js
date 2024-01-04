f882b63a04e871ba93c7e81763ea6dbd6778fb32
songhaetnim <minshinzz@naver.com>
2024-01-04 09:44:34 +0900

    jkjk

--- javascript/ch03.조건문/Q1.alarm.js
+++ javascript/ch03.조건문/Q1.alarm.js
@@ -1,19 +1,19 @@
-//백준도장 28847
-//알람 시계
- let hour =6, minute = 30;
- let newHour =0, newMin = 0;
+// 백준도장 2884
+// 알람시계
+let hour = 6, minute = 30;
+let newHour = 0, newMinute = 0;
 
- if (minuet >= 45){
-     newHour = hour;
-     newMin = minute - 45;
- } else {
-    if (hour == 0) {
-        newHour =23;
-        newMin = minute + 60 -45;
-}   else {
-    newHour = hour -1;
-    newMin =minute + 60 -45;
+if (minute >= 45) {
+    newHour = hour;
+    newMinute = minute - 45;
+} else {
+    if (hour == 0){
+        newHour = 23;
+        newMinute = minute + 60 - 45 ;
+    } else {
+        newHour = hour - 1;
+        newMinute = minute + 60 - 45 ;
+    }
 }
- }
- console.log(`${hour}시 ${minute}분 -->${newHour}시 ${newMin}분`);
- 
+
+console.log(`${hour}시 ${minute}분 --> ${newHour}시 ${newMinute}분}`)
